50439b92df19d041189c6fcb79cdb7ddd1378178
Jack Sheriff <jsheriff@mitre.org>
2021-01-13 12:05:38 -0500

    Allow request bodies up to 50mb. Use routes for collections and collection bundles.

--- app/models/subschemas/x-mitre-collection.js
+++ app/models/subschemas/x-mitre-collection.js
@@ -1,13 +1,13 @@
 'use strict';
 
+const xMitreContent = {
+    object_ref: { type: String, required: true },
+    object_modified : { type: Date, required: true }
+};
+
 module.exports.xMitreCollection = {
     name: { type: String, required: true },
     description: String,
     x_mitre_content: [ xMitreContent ],
     x_mitre_version: String
-};
-
-const xMitreContent = {
-    object_ref: { type: String, required: true },
-    object_modified : { type: Date, required: true }
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,8 @@ const techniquesRoutes = require('./techniques-routes');
 const tacticsRoutes = require('./tactics-routes');
 const groupsRoutes = require('./groups-routes');
 const collectionIndexesRoutes = require('./collection-indexes-routes');
+const collectionsRoutes = require('./collections-routes');
+const collectionBundlesRoutes = require('./collection-bundles-routes');
 
 const errorHandler = require('../lib/error-handler');
 const config = require('../config/config');
@@ -15,7 +17,7 @@ const config = require('../config/config');
 const router = express.Router();
 
 // Parse the request body
-router.use('/api', bodyParser.json({limit: '1mb'}));
+router.use('/api', bodyParser.json({limit: '50mb'}));
 router.use('/api', bodyParser.urlencoded({ limit: '1mb', extended: true }));
 
 // Setup request validation
@@ -30,6 +32,8 @@ router.use('/api', techniquesRoutes);
 router.use('/api', tacticsRoutes);
 router.use('/api', groupsRoutes);
 router.use('/api', collectionIndexesRoutes);
+router.use('/api', collectionsRoutes);
+router.use('/api', collectionBundlesRoutes);
 
 // Handle errors that haven't otherwise been caught
 router.use(errorHandler.bodyParser);
