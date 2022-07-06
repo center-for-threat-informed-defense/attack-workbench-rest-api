47de57b792a48f66a9cfc132347b8c01f49d6a72
Jack Sheriff <jsheriff@mitre.org>
2022-07-06 17:20:55 -0400

    Fix errors in move relationships migration action.

--- migrations/20220705205741-move-relationships.js
+++ migrations/20220705205741-move-relationships.js
@@ -6,20 +6,22 @@ module.exports = {
         const relationshipsCollection = await db.collection('relationships');
 
         // Move relationship objects from the attackObjects collection to the relationships collection
-        const oldRelationships = await attackObjectsCollection.find({ '__t': 'relationship' }).toArray();
+        const oldRelationships = await attackObjectsCollection.find({ '__t': 'RelationshipModel' }).toArray();
+        console.log(`Found ${ oldRelationships.length } relationships in the attackObjects collection`);
 
         for (const relationship of oldRelationships) {
             // The relationship should not exist in the relationships collection
-            const newRelationship = await relationshipsCollection.find({ 'stix.id': relationship.stix.id, 'stix.modified': relationship.stix.modified });
+            const newRelationship = await relationshipsCollection.findOne({ 'stix.id': relationship.stix.id, 'stix.modified': relationship.stix.modified });
             if (newRelationship) {
-                console.warning('Relationship already exists in relationships collection. Will delete from attackObjects collection without adding to relationships collection.');
+                console.log('Relationship already exists in relationships collection. Will delete from attackObjects collection without adding to relationships collection.');
                 attackObjectsCollection.findOneAndDelete({ 'stix.id': relationship.stix.id, 'stix.modified': relationship.stix.modified });
             }
             else {
+//                console.log(`Moving relationships ${ relationship.stix.id }/${ relationship.stix.modified }`);
                 // Add the relationship to the relationships collection
                 relationship._id = undefined;
                 relationship.__t = undefined;
-                relationshipsCollection.insert(relationship);
+                relationshipsCollection.insertOne(relationship);
 
                 // Remove the relationship from the attackObjects collection
                 attackObjectsCollection.findOneAndDelete({ 'stix.id': relationship.stix.id, 'stix.modified': relationship.stix.modified });
@@ -33,19 +35,20 @@ module.exports = {
 
         // Move relationship objects from the relationship collection to the attackObjects collection
         const newRelationships = await relationshipsCollection.find({ }).toArray();
+        console.log(`Found ${ newRelationships.length } relationships in the relationships collection`);
 
         for (const relationship of newRelationships) {
             // The relationship should not exist in the attackObjects collection
-            const oldRelationship = await attackObjectsCollection.find({ 'stix.id': relationship.stix.id, 'stix.modified': relationship.stix.modified });
+            const oldRelationship = await attackObjectsCollection.findOne({ 'stix.id': relationship.stix.id, 'stix.modified': relationship.stix.modified });
             if (oldRelationship) {
-                console.warning('Relationship already exists in attackObjects collection. Will delete from attackObjects collection without adding to attackObjects collection.');
+                console.log('Relationship already exists in attackObjects collection. Will delete from attackObjects collection without adding to attackObjects collection.');
                 relationshipsCollection.findOneAndDelete({ 'stix.id': relationship.stix.id, 'stix.modified': relationship.stix.modified });
             }
             else {
                 // Add the relationship to the attackObjects collection
                 relationship._id = undefined;
-                relationship.__t = undefined;
-                attackObjectsCollection.insert(relationship);
+                relationship.__t = 'RelationshipModel';
+                attackObjectsCollection.insertOne(relationship);
 
                 // Remove the relationship from the relationships collection
                 relationshipsCollection.findOneAndDelete({ 'stix.id': relationship.stix.id, 'stix.modified': relationship.stix.modified });
