a2b8150309ebd3c4659b905a62e449e2fc177658
Vincent Minnocci <vminnocci@mitre.org>
2023-04-26 14:09:47 -0400

    add teams model

---
+++ app/models/team-model.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// Create the definition
+const teamDefinition = {
+    id: { type: String, unique: true },
+    name: { type: String, required: true },
+    description: { type: String },
+    userIDs: [ String ],
+    created: { type: Date, required: true },
+    modified: { type: Date, required: true }
+};
+
+// Create the schema
+const teamSchema = new mongoose.Schema(teamDefinition, { bufferCommands: false });
+
+// Create the model
+const TeamModel = mongoose.model('Team', teamSchema);
+
+module.exports = TeamModel;
