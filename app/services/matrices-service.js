90fbb4320899fe89cf095728351e4c592b6ea0ec
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-08-29 15:57:47 -0400

    Refactor matrices-service to use new exceptions module

--- app/services/matrices-service.js
+++ app/services/matrices-service.js
@@ -10,92 +10,99 @@ const config = require('../config/config');
 const regexValidator = require('../lib/regex');
 const {lastUpdatedByQueryHelper} = require('../lib/request-parameter-helper');
 const matrixRepository = require('../repository/matrix-repository');
-
-const ErrorType = {
-    MissingParameter: 'Missing required parameter',
-    BadlyFormattedParameter: 'Badly formatted parameter',
-    DuplicateId: 'Duplicate id',
-    NotFound: 'Document not found',
-    InvalidQueryStringParameter: 'Invalid query string parameter'
-};
-
-function processError(errorType, options = {}) {
-    if (!ErrorType[errorType]) {
-        throw new Error('Unknown error type provided to processError function.');
-    }
-
-    const error = new Error(ErrorType[errorType]);
-
-    // Apply options (if defined) to the error object
-    for (const key in options) {
-        if (Object.prototype.hasOwnProperty.call(options, key)) {
-            error[key] = options[key];
-        }
-    }
-
-    throw error;
-};
-
-// TODO why is this being exported?
-// exports.errors = ErrorType;
+const Errors = require('../exceptions');
+const logger = require('../lib/logger');
 
 exports.retrieveAll = async function (options) {
+    let results;
     try {
-        const results = await matrixRepository.retrieveAll(options);
-
-        await identitiesService.addCreatedByAndModifiedByIdentitiesToAll(results[0].documents);
-
-        if (options.includePagination) {
-            let derivedTotalCount = 0;
-            if (results[0].totalCount.length > 0) {
-                derivedTotalCount = results[0].totalCount[0].totalCount;
-            }
-            const returnValue = {
-                pagination: {
-                    total: derivedTotalCount,
-                    offset: options.offset,
-                    limit: options.limit
-                },
-                data: results[0].documents
-            };
-            return returnValue;
-        } else {
-            return results[0].documents;
-        }
+        results = await matrixRepository.retrieveAll(options);
     } catch (err) {
-        throw err;
+        throw new Errors.DatabaseError({ detail: 'Failed to retrieve records from the matrix repository.' });
+    }
+
+    try {
+        await identitiesService.addCreatedByAndModifiedByIdentitiesToAll(results[0].documents);
+    } catch (err) {
+        throw new Errors.IdentityServiceError({ detail: 'Failed to add identities to documents.' });
+    }
+
+    logger.debug(`Retrieved ${results.length} matrices`);
+    logger.debug(util.inspect(results, { depth: 5 }));
+    logger.debug(util.inspect(results[0].documents[0], { depth: 5 }));
+
+    if (options.includePagination) {
+        let derivedTotalCount = 0;
+        if (results.length >= 1 && results[0].totalCount && results[0].totalCount.length > 0) {
+            derivedTotalCount = results[0].totalCount[0].totalCount;
+        }
+        const returnValue = {
+            pagination: {
+                total: derivedTotalCount,
+                offset: options.offset,
+                limit: options.limit
+            },
+            data: results[0].documents
+        };
+        return returnValue;
+    } else {
+        return results[0].documents;
     }
 };
 
 exports.retrieveById = async function (stixId, options) {
     if (!stixId) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'stixId' });
+        throw new Errors.MissingParameterError({ parameterName: 'stixId' });
     }
 
-    if (options.versions === 'all') {
-        const matrices = await matrixRepository.retrieveAllByStixId(stixId);
-        await identitiesService.addCreatedByAndModifiedByIdentitiesToAll(matrices);
-        return matrices;
-    } else if (options.versions === 'latest') {
-        const matrix = await matrixRepository.retrieveLatestByStixId(stixId);
-        if (matrix) {
-            await identitiesService.addCreatedByAndModifiedByIdentities(matrix);
-            return [matrix];
+    try {
+        if (options.versions === 'all') {
+            const matrices = await matrixRepository.retrieveAllByStixId(stixId);
+
+            try {
+                await identitiesService.addCreatedByAndModifiedByIdentitiesToAll(matrices);
+            } catch (err) {
+                throw new Errors.IdentityServiceError({ detail: 'Failed to add identities to all matrices.' });
+            }
+
+            return matrices;
+
+        } else if (options.versions === 'latest') {
+            const matrix = await matrixRepository.retrieveLatestByStixId(stixId);
+
+            if (matrix) {
+                try {
+                    await identitiesService.addCreatedByAndModifiedByIdentities(matrix);
+                } catch (err) {
+                    throw new Errors.IdentityServiceError({ detail: 'Failed to add identities to the latest matrix.' });
+                }
+
+                return [matrix];
+            } else {
+                return [];
+            }
+
         } else {
-            return [];
+            throw new Errors.InvalidQueryStringParameterError({ parameterName: 'versions' });
         }
-    } else {
-        processError(ErrorType.InvalidQueryStringParameter, { parameterName: 'versions' });
+    } catch (err) {
+        // If the error is one of our custom errors, re-throw it.
+        // Otherwise, encapsulate it in a more general error (optional).
+        if (err instanceof Errors.CustomError) {
+            throw err;
+        } else {
+            throw new Errors.DatabaseError({ detail: 'Failed during matrix retrieval by ID.', originalError: err.message });
+        }
     }
 };
 
 exports.retrieveVersionById = async function (stixId, modified) {
     if (!stixId) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'stixId' });
+        throw new Errors.MissingParameterError({ parameterName: 'stixId' });
     }
 
     if (!modified) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'modified' });
+        throw new Errors.MissingParameterError({ parameterName: 'modified' });
     }
 
     try {
@@ -103,52 +110,78 @@ exports.retrieveVersionById = async function (stixId, modified) {
 
         if (!matrix) {
             console.log('** NOT FOUND');
-            return null; // TODO should we return NotFound here?
+            // TODO determine if we should throw an error here instead of returning null
+            // throw new Errors.NotFoundError({ detail: 'Matrix not found for given stixId and modified date.' });
+            return null;
         } else {
-            await identitiesService.addCreatedByAndModifiedByIdentities(matrix);
+            try {
+                await identitiesService.addCreatedByAndModifiedByIdentities(matrix);
+            } catch (err) {
+                throw new Errors.IdentityServiceError({ detail: 'Failed to add identities to the matrix.' });
+            }
+
             return matrix;
         }
     } catch (err) {
-        throw err;
+        if (err instanceof Errors.CustomError) {
+            throw err;
+        } else {
+            throw new Errors.DatabaseError({ detail: 'Failed during matrix retrieval by version and ID.', originalError: err.message });
+        }
     }
 };
 
 
 let retrieveTacticById;
 let retrieveTechniquesForTactic;
+
 exports.retrieveTechniquesForMatrix = async function (stixId, modified) {
-    if (!retrieveTacticById) {
+    // Lazy loading of services
+    if (!retrieveTacticById || !retrieveTechniquesForTactic) {
         const tacticsService = require('./tactics-service');
         retrieveTacticById = util.promisify(tacticsService.retrieveById);
-    }
-    if (!retrieveTechniquesForTactic) {
-        const tacticsService = require('./tactics-service');
         retrieveTechniquesForTactic = tacticsService.retrieveTechniquesForTactic;
     }
 
     if (!stixId) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'stixId' });
+        throw new Errors.MissingParameterError({ parameterName: 'stixId' });
     }
     if (!modified) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'modified' });
+        throw new Errors.MissingParameterError({ parameterName: 'modified' });
     }
 
-    const matrix = await matrixRepository.retrieveByVersion(stixId, modified);
+    let matrix;
+    try {
+        matrix = await matrixRepository.retrieveByVersion(stixId, modified);
+    } catch (err) {
+        throw new Errors.DatabaseError({ detail: 'Failed during matrix retrieval by version and ID.', originalError: err.message });
+    }
 
     if (!matrix) {
-        return; // TODO should we return NotFound here?
+        // TODO determine if we should throw an error here instead of returning null
+        // throw new Errors.NotFoundError({ detail: 'Matrix not found for given stixId and modified date.' });
+        return null;
     }
 
     const options = { versions: 'latest', offset: 0, limit: 0 };
     const tacticsTechniques = {};
 
     for (const tacticId of matrix.stix.tactic_refs) {
-        const tactics = await retrieveTacticById(tacticId, options);
-        if (tactics.length) {
+        let tactics, techniques;
+        try {
+            tactics = await retrieveTacticById(tacticId, options);
+            if (tactics && tactics.length) {
+                techniques = await retrieveTechniquesForTactic(tacticId, tactics[0].stix.modified, options);
+            }
+        } catch (err) {
+            throw new Errors.ServiceError({ detail: 'Error while retrieving tactics or techniques.', originalError: err.message });
+        }
+
+        if (tactics && tactics.length) {
             const tactic = tactics[0];
-            const techniques = await retrieveTechniquesForTactic(tacticId, tactic.stix.modified, options);
             const parentTechniques = [];
             const subtechniques = [];
+
             for (const technique of techniques) {
                 if (!technique.stix.x_mitre_is_subtechnique) {
                     parentTechniques.push(technique);
@@ -156,6 +189,7 @@ exports.retrieveTechniquesForMatrix = async function (stixId, modified) {
                     subtechniques.push(technique);
                 }
             }
+
             for (const parentTechnique of parentTechniques) {
                 parentTechnique.subtechniques = [];
                 for (const subtechnique of subtechniques) {
@@ -168,82 +202,81 @@ exports.retrieveTechniquesForMatrix = async function (stixId, modified) {
             tacticsTechniques[tactic.stix.name] = tactic;
         }
     }
+
     return tacticsTechniques;
 };
 
 
 exports.createIsAsync = true;
-exports.create = async function(data, options) {
-    // This function handles two use cases:
-    //   1. This is a completely new object. Create a new object and generate the stix.id if not already
-    //      provided. Set both stix.created_by_ref and stix.x_mitre_modified_by_ref to the organization identity.
-    //   2. This is a new version of an existing object. Create a new object with the specified id.
-    //      Set stix.x_mitre_modified_by_ref to the organization identity.
-
-    // Create the document
-    const matrix = new Matrix(data);
-
-    options = options || {};
-    if (!options.import) {
-        // Set the ATT&CK Spec Version
-        matrix.stix.x_mitre_attack_spec_version = matrix.stix.x_mitre_attack_spec_version ?? config.app.attackSpecVersion;
-
-        // Record the user account that created the object
-        if (options.userAccountId) {
-            matrix.workspace.workflow.created_by_user_account = options.userAccountId;
-        }
-
-        // Set the default marking definitions
-        await attackObjectsService.setDefaultMarkingDefinitions(matrix);
-
-        // Get the organization identity
-        const organizationIdentityRef = await systemConfigurationService.retrieveOrganizationIdentityRef();
-
-        // Check for an existing object
-        let existingObject;
-        if (matrix.stix.id) {
-            // existingObject = await Matrix.findOne({ 'stix.id': matrix.stix.id });
-            existingObject = await matrixRepository.retrieveAllByStixId(matrix.stix.id);
-        }
-
-        if (existingObject) {
-            // New version of an existing object
-            // Only set the x_mitre_modified_by_ref property
-            matrix.stix.x_mitre_modified_by_ref = organizationIdentityRef;
-        }
-        else {
-            // New object
-            // Assign a new STIX id if not already provided
-            matrix.stix.id = matrix.stix.id || `x-mitre-matrix--${uuid.v4()}`;
-
-            // Set the created_by_ref and x_mitre_modified_by_ref properties
-            matrix.stix.created_by_ref = organizationIdentityRef;
-            matrix.stix.x_mitre_modified_by_ref = organizationIdentityRef;
-        }
-    }
-
-    // Save the document in the database
+// TODO refactor processError and try/catch
+exports.create = async function (data, options) {
     try {
+        // This function handles two use cases:
+        //   1. This is a completely new object. Create a new object and generate the stix.id if not already
+        //      provided. Set both stix.created_by_ref and stix.x_mitre_modified_by_ref to the organization identity.
+        //   2. This is a new version of an existing object. Create a new object with the specified id.
+        //      Set stix.x_mitre_modified_by_ref to the organization identity.
+
+        // Create the document
+        const matrix = new Matrix(data);
+
+        options = options || {};
+        if (!options.import) {
+            // Set the ATT&CK Spec Version
+            matrix.stix.x_mitre_attack_spec_version = matrix.stix.x_mitre_attack_spec_version ?? config.app.attackSpecVersion;
+
+            // Record the user account that created the object
+            if (options.userAccountId) {
+                matrix.workspace.workflow.created_by_user_account = options.userAccountId;
+            }
+
+            // Set the default marking definitions
+            await attackObjectsService.setDefaultMarkingDefinitions(matrix);
+
+            // Get the organization identity
+            const organizationIdentityRef = await systemConfigurationService.retrieveOrganizationIdentityRef();
+
+            // Check for an existing object
+            let existingObject;
+            if (matrix.stix.id) {
+                // existingObject = await Matrix.findOne({ 'stix.id': matrix.stix.id });
+                existingObject = await matrixRepository.retrieveAllByStixId(matrix.stix.id);
+            }
+
+            if (existingObject) {
+                // New version of an existing object
+                // Only set the x_mitre_modified_by_ref property
+                matrix.stix.x_mitre_modified_by_ref = organizationIdentityRef;
+            }
+            else {
+                // New object
+                // Assign a new STIX id if not already provided
+                matrix.stix.id = matrix.stix.id || `x-mitre-matrix--${uuid.v4()}`;
+
+                // Set the created_by_ref and x_mitre_modified_by_ref properties
+                matrix.stix.created_by_ref = organizationIdentityRef;
+                matrix.stix.x_mitre_modified_by_ref = organizationIdentityRef;
+            }
+        }
         return await matrixRepository.saveMatrix(matrix);
-    }
-    catch (err) {
+    } catch (err) {
         if (err.name === 'MongoServerError' && err.code === 11000) {
-            // 11000 = Duplicate index
-            return processError(ErrorType.DuplicateId);
+            // Duplicate key error
+            throw new Errors.DuplicateIdError({
+                detail: `Matrix with id '${matrix.stix.id}' already exists.`
+            });
         }
-        else {
-            throw err;
-        }
+        throw err;
     }
 };
 
 exports.updateFull = async function (stixId, stixModified, data) {
 
     if (!stixId) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'stixId' });
+        throw new Errors.MissingParameterError({ parameterName: 'stixId' });
     }
-    if (!modified) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'modified' });
+    if (!stixModified) {
+        throw new Errors.MissingParameterError({ parameterName: 'modified' });
     }
 
     try {
@@ -257,22 +290,20 @@ exports.updateFull = async function (stixId, stixModified, data) {
         return await matrixRepository.updateAndSave(document, data);
     } catch (err) {
         if (err.name === 'CastError') {
-            return processError(ErrorType.BadlyFormattedParameter, { parameterName: 'stixId' });
+            throw new Errors.BadlyFormattedParameterError({ parameterName: 'stixId' });
         } else if (err.name === 'MongoServerError' && err.code === 11000) {
-            // 11000 = Duplicate index
-            return processError(ErrorType.DuplicateId);
-        } else {
-            throw err;
+            throw new Errors.DuplicateIdError();
         }
+        throw err;
     }
 };
 
 exports.deleteVersionById = async function (stixId, stixModified) {
     if (!stixId) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'stixId' });
+        throw new Errors.MissingParameterError({ parameterName: 'stixId' });
     }
     if (!modified) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'modified' });
+        throw new Errors.MissingParameterError({ parameterName: 'modified' });
     }
     try {
         const matrix = await matrixRepository.findOneAndRemove(stixId, stixModified);
@@ -290,7 +321,7 @@ exports.deleteVersionById = async function (stixId, stixModified) {
 
 exports.deleteById = async function (stixId) {
     if (!stixId) {
-        return processError(ErrorType.MissingParameter, { parameterName: 'stixId' });
+        return new Errors.MissingParameterError({ parameterName: 'stixId' });
     }
     try {
         return await matrixRepository.deleteMany(stixId);
