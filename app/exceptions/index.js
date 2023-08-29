bdb3feb9d8bde94aaa4f3b31759bf37ac8f0d9d7
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-08-29 15:57:05 -0400

    Add new exceptions module to store custom Exception classes

---
+++ app/exceptions/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+class CustomError extends Error {
+    constructor(message, options = {}) {
+        super(message);
+
+        // Apply options (if defined) to the error object
+        for (const key in options) {
+            if (Object.prototype.hasOwnProperty.call(options, key)) {
+                this[key] = options[key];
+            }
+        }
+    }
+}
+
+class MissingParameterError extends CustomError {
+    constructor(options) {
+        super('Missing required parameter', options);
+    }
+}
+
+class BadlyFormattedParameterError extends CustomError {
+    constructor(options) {
+        super('Badly formatted parameter', options);
+    }
+}
+
+class DuplicateIdError extends CustomError {
+    constructor(options) {
+        super('Duplicate id', options);
+    }
+}
+
+class NotFoundError extends CustomError {
+    constructor(options) {
+        super('Document not found', options);
+    }
+}
+
+class InvalidQueryStringParameterError extends CustomError {
+    constructor(options) {
+        super('Invalid query string parameter', options);
+    }
+}
+
+class IdentityServiceError extends CustomError {
+    constructor(options) {
+        super('An error occurred in the identities service.', options);
+    }
+}
+
+module.exports = {
+    MissingParameterError,
+    BadlyFormattedParameterError,
+    DuplicateIdError,
+    NotFoundError,
+    InvalidQueryStringParameterError,
+    IdentityServiceError
+};
