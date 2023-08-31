d56b486030cc9ae109704f1b1c3e8b005d9c0210
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-08-31 14:39:34 -0400

    Remove unnecessary DTO classes

--- app/dto/repository-response-dto.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class RepositoryResponseDTO {
-    constructor(data) {
-        this.totalCount = data.totalCount || [];
-        this.documents = data.documents || [];
-    }
-}
-module.exports = RepositoryResponseDTO;
