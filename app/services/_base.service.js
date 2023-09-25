b38905d5bd39a54c7477762a33adfa5da8c08401
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-09-25 17:00:27 -0400

    Migrate pagination module to static method in _base.service

--- app/services/_base.service.js
+++ app/services/_base.service.js
@@ -21,6 +21,25 @@ class BaseService extends AbstractService {
         this.model = model;
     }
 
+    static paginate(options, results) {
+        if (options.includePagination) {
+            let derivedTotalCount = 0;
+            if (results[0].totalCount && results[0].totalCount.length > 0) {
+                derivedTotalCount = results[0].totalCount[0].totalCount;
+            }
+            return {
+                pagination: {
+                    total: derivedTotalCount,
+                    offset: options.offset,
+                    limit: options.limit
+                },
+                data: results[0].documents
+            };
+        } else {
+            return results[0].documents;
+        }
+    }
+
     async retrieveAll(options) {
         let results;
         try {
