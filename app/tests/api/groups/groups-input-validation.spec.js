1e07ea34473ee72f84d36f1d890567430eaca70c
Sun <vsun@mitre.org>
2023-10-04 17:55:27 -0400

    refactoring tests part 1

--- app/tests/api/groups/groups-input-validation.spec.js
+++ app/tests/api/groups/groups-input-validation.spec.js
@@ -36,7 +36,7 @@ const initialObjectData = {
 function executeTests(getApp, propertyName, options) {
     options = options ?? {};
     if (options.required) {
-        it(`POST /api/groups does not create a group when ${propertyName} is missing`, function (done) {
+        it(`POST /api/groups does not create a group when ${propertyName} is missing`, async function () {
             const groupData = _.cloneDeep(initialObjectData);
             const timestamp = new Date().toISOString();
             groupData.stix.created = timestamp;
@@ -47,18 +47,11 @@ function executeTests(getApp, propertyName, options) {
                 .post('/api/groups')
                 .send(body)
                 .set('Accept', 'application/json')
-                .expect(400)
-                .end(function (err, res) {
-                    if (err) {
-                        done(err);
-                    } else {
-                        done();
-                    }
-                });
+                .expect(400);
         });
     }
 
-    it(`POST /api/groups does not create a group when ${ propertyName } is a number`, function (done) {
+    it(`POST /api/groups does not create a group when ${ propertyName } is a number`, async function () {
         const groupData = _.cloneDeep(initialObjectData);
         const timestamp = new Date().toISOString();
         groupData.stix.created = timestamp;
@@ -69,18 +62,10 @@ function executeTests(getApp, propertyName, options) {
             .post('/api/groups')
             .send(body)
             .set('Accept', 'application/json')
-            .expect(400)
-            .end(function(err, res) {
-                if (err) {
-                    done(err);
-                }
-                else {
-                    done();
-                }
-            });
+            .expect(400);
     });
 
-    it(`POST /api/groups does not create a group when ${ propertyName } is an object`, function (done) {
+    it(`POST /api/groups does not create a group when ${ propertyName } is an object`, async function () {
         const groupData = _.cloneDeep(initialObjectData);
         const timestamp = new Date().toISOString();
         groupData.stix.created = timestamp;
@@ -91,15 +76,7 @@ function executeTests(getApp, propertyName, options) {
             .post('/api/groups')
             .send(body)
             .set('Accept', 'application/json')
-            .expect(400)
-            .end(function(err, res) {
-                if (err) {
-                    done(err);
-                }
-                else {
-                    done();
-                }
-            });
+            .expect(400);
     });
 }
 
@@ -125,25 +102,17 @@ describe('Groups API Input Validation', function () {
         passportCookie = await login.loginAnonymous(app);
     });
 
-    it('POST /api/groups does not create an empty group', function (done) {
+    it('POST /api/groups does not create an empty group', async function () {
         const body = { };
         request(app)
             .post('/api/groups')
             .send(body)
             .set('Accept', 'application/json')
             .set('Cookie', `${ login.passportCookieName }=${ passportCookie.value }`)
-            .expect(400)
-            .end(function(err, res) {
-                if (err) {
-                    done(err);
-                }
-                else {
-                    done();
-                }
-            });
+            .expect(400);
     });
 
-    it('POST /api/groups does not create a group when an unknown query parameter is provided', function (done) {
+    it('POST /api/groups does not create a group when an unknown query parameter is provided', async function () {
         const groupData = _.cloneDeep(initialObjectData);
         const timestamp = new Date().toISOString();
         groupData.stix.created = timestamp;
@@ -154,18 +123,10 @@ describe('Groups API Input Validation', function () {
             .send(body)
             .set('Accept', 'application/json')
             .set('Cookie', `${ login.passportCookieName }=${ passportCookie.value }`)
-            .expect(400)
-            .end(function(err, res) {
-                if (err) {
-                    done(err);
-                }
-                else {
-                    done();
-                }
-            });
+            .expect(400);
     });
 
-    it('POST /api/groups does not create a group when an invalid type is provided', function (done) {
+    it('POST /api/groups does not create a group when an invalid type is provided', function () {
         const groupData = _.cloneDeep(initialObjectData);
         const timestamp = new Date().toISOString();
         groupData.stix.created = timestamp;
@@ -177,18 +138,10 @@ describe('Groups API Input Validation', function () {
             .send(body)
             .set('Accept', 'application/json')
             .set('Cookie', `${ login.passportCookieName }=${ passportCookie.value }`)
-            .expect(400)
-            .end(function(err, res) {
-                if (err) {
-                    done(err);
-                }
-                else {
-                    done();
-                }
-            });
+            .expect(400);
     });
 
-    it('POST /api/groups does not create a group when an incorrect type is provided', function (done) {
+    it('POST /api/groups does not create a group when an incorrect type is provided', function () {
         const groupData = _.cloneDeep(initialObjectData);
         const timestamp = new Date().toISOString();
         groupData.stix.created = timestamp;
@@ -200,15 +153,7 @@ describe('Groups API Input Validation', function () {
             .send(body)
             .set('Accept', 'application/json')
             .set('Cookie', `${ login.passportCookieName }=${ passportCookie.value }`)
-            .expect(400)
-            .end(function(err, res) {
-                if (err) {
-                    done(err);
-                }
-                else {
-                    done();
-                }
-            });
+            .expect(400);
     });
 
     executeTests(() => app, 'stix.type', { required: true });
