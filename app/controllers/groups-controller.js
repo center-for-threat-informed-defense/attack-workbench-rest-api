496b7c6f5f26a628cb0155a15751cc474823f84e
Sun <vsun@mitre.org>
2023-10-05 14:38:37 -0400

    refactored groups controller

--- app/controllers/groups-controller.js
+++ app/controllers/groups-controller.js
@@ -3,7 +3,7 @@
 const groupsService = require('../services/groups-service');
 const logger = require('../lib/logger');
 
-exports.retrieveAll = function(req, res) {
+exports.retrieveAll = async function(req, res) {
     const options = {
         offset: req.query.offset || 0,
         limit: req.query.limit || 0,
@@ -14,13 +14,9 @@ exports.retrieveAll = function(req, res) {
         lastUpdatedBy: req.query.lastUpdatedBy,
         includePagination: req.query.includePagination
     }
+    try {
+            const res = await groupsService.retrieveAll(options);
 
-    groupsService.retrieveAll(options, function(err, results) {
-        if (err) {
-            logger.error('Failed with error: ' + err);
-            return res.status(500).send('Unable to get groups. Server error.');
-        }
-        else {
             if (options.includePagination) {
                 logger.debug(`Success: Retrieved ${ results.data.length } of ${ results.pagination.total } total group(s)`);
             }
@@ -28,75 +24,80 @@ exports.retrieveAll = function(req, res) {
                 logger.debug(`Success: Retrieved ${ results.length } group(s)`);
             }
             return res.status(200).send(results);
+        }  catch (err) {
+            logger.error('Failed with error: ' + err);
+            return res.status(500).send('Unable to get groups. Server error.');
         }
-    });
+  
 };
 
-exports.retrieveById = function(req, res) {
+exports.retrieveById = async function(req, res) {
     const options = {
         versions: req.query.versions || 'latest'
     }
+    try {
+        const groups = await groupsService.retrieveById(req.params.stixId, options);
 
-    groupsService.retrieveById(req.params.stixId, options, function (err, groups) {
-        if (err) {
-            if (err.message === groupsService.errors.badlyFormattedParameter) {
-                logger.warn('Badly formatted stix id: ' + req.params.stixId);
-                return res.status(400).send('Stix id is badly formatted.');
-            }
-            else if (err.message === groupsService.errors.invalidQueryStringParameter) {
-                logger.warn('Invalid query string: versions=' + req.query.versions);
-                return res.status(400).send('Query string parameter versions is invalid.');
-            }
-            else {
-                logger.error('Failed with error: ' + err);
-                return res.status(500).send('Unable to get groups. Server error.');
-            }
+    
+        if (groups.length === 0) {
+            return res.status(404).send('Group not found.');
         }
         else {
-            if (groups.length === 0) {
-                return res.status(404).send('Group not found.');
-            }
-            else {
-                logger.debug(`Success: Retrieved ${ groups.length } group(s) with id ${ req.params.stixId }`);
-                return res.status(200).send(groups);
-            }
+            logger.debug(`Success: Retrieved ${ groups.length } group(s) with id ${ req.params.stixId }`);
+            return res.status(200).send(groups);
         }
-    });
+
+    } catch (err) {
+        if (err.message === groupsService.errors.badlyFormattedParameter) {
+            logger.warn('Badly formatted stix id: ' + req.params.stixId);
+            return res.status(400).send('Stix id is badly formatted.');
+        }
+        else if (err.message === groupsService.errors.invalidQueryStringParameter) {
+            logger.warn('Invalid query string: versions=' + req.query.versions);
+            return res.status(400).send('Query string parameter versions is invalid.');
+        }
+        else {
+            logger.error('Failed with error: ' + err);
+            return res.status(500).send('Unable to get groups. Server error.');
+        }
+    }
+        
 };
 
-exports.retrieveVersionById = function(req, res) {
-    groupsService.retrieveVersionById(req.params.stixId, req.params.modified, function (err, group) {
-        if (err) {
-            if (err.message === groupsService.errors.badlyFormattedParameter) {
-                logger.warn('Badly formatted stix id: ' + req.params.stixId);
-                return res.status(400).send('Stix id is badly formatted.');
-            }
-            else {
-                logger.error('Failed with error: ' + err);
-                return res.status(500).send('Unable to get group. Server error.');
-            }
-        } else {
-            if (!group) {
-                return res.status(404).send('Group not found.');
-            }
-            else {
-                logger.debug(`Success: Retrieved group with id ${group.id}`);
-                return res.status(200).send(group);
-            }
+exports.retrieveVersionById = async function(req, res) {
+   
+    try {
+        const group =  groupsService.retrieveVersionById(req.params.stixId, req.params.modified);
+        if (!group) {
+            return res.status(404).send('Group not found.');
         }
-    });
+    else {
+        logger.debug(`Success: Retrieved group with id ${group.id}`);
+        return res.status(200).send(group);
+    }
+    } catch (err) {
+        if (err.message === groupsService.errors.badlyFormattedParameter) {
+            logger.warn('Badly formatted stix id: ' + req.params.stixId);
+            return res.status(400).send('Stix id is badly formatted.');
+        }
+        else {
+            logger.error('Failed with error: ' + err);
+            return res.status(500).send('Unable to get group. Server error.');
+        }
+    } 
+
 };
 
 exports.create = async function(req, res) {
     // Get the data from the request
     const groupData = req.body;
+    const options = {
+        import: false,
+        userAccountId: req.user?.userAccountId
+    };
 
     // Create the group
     try {
-        const options = {
-            import: false,
-            userAccountId: req.user?.userAccountId
-        };
 
         const group = await groupsService.create(groupData, options);
         logger.debug('Success: Created group with id ' + group.stix.id);
@@ -118,58 +119,57 @@ exports.create = async function(req, res) {
     }
 };
 
-exports.updateFull = function(req, res) {
+exports.updateFull = async function(req, res) {
     // Get the data from the request
     const groupData = req.body;
 
-    // Create the group
-    groupsService.updateFull(req.params.stixId, req.params.modified, groupData, function(err, group) {
-        if (err) {
-            logger.error("Failed with error: " + err);
-            return res.status(500).send("Unable to update group. Server error.");
+    try {
+        // Create the group
+        const group = await groupsService.updateFull(req.params.stixId, req.params.modified, groupData);
+        if (!group) {
+            return res.status(404).send('Group not found.');
+        } else {
+            logger.debug("Success: Updated group with id " + group.stix.id);
+            return res.status(200).send(group);
         }
-        else {
-            if (!group) {
-                return res.status(404).send('Group not found.');
-            } else {
-                logger.debug("Success: Updated group with id " + group.stix.id);
-                return res.status(200).send(group);
-            }
-        }
-    });
+    } catch (err) {
+        logger.error("Failed with error: " + err);
+        return res.status(500).send("Unable to update group. Server error.");
+    }
+
 };
 
-exports.deleteVersionById = function(req, res) {
-    groupsService.deleteVersionById(req.params.stixId, req.params.modified, function (err, group) {
-        if (err) {
-            logger.error('Delete group failed. ' + err);
-            return res.status(500).send('Unable to delete group. Server error.');
+exports.deleteVersionById = async function(req, res) {
+
+    try {
+        const group = groupsService.deleteVersionById(req.params.stixId, req.params.modified);
+        if (!group) {
+            return res.status(404).send('Group not found.');
+        } else {
+            logger.debug("Success: Deleted group with id " + group.stix.id);
+            return res.status(204).end();
         }
-        else {
-            if (!group) {
-                return res.status(404).send('Group not found.');
-            } else {
-                logger.debug("Success: Deleted group with id " + group.stix.id);
-                return res.status(204).end();
-            }
-        }
-    });
+    }  catch (err) {
+        logger.error('Delete group failed. ' + err);
+        return res.status(500).send('Unable to delete group. Server error.');
+    }
+
 };
 
-exports.deleteById = function(req, res) {
-    groupsService.deleteById(req.params.stixId, function (err, groups) {
-        if (err) {
-            logger.error('Delete group failed. ' + err);
-            return res.status(500).send('Unable to delete group. Server error.');
+exports.deleteById = async function(req, res) {
+    
+    try {
+        const groups = groupsService.deleteById(req.params.stixId);
+        if (groups.deletedCount === 0) {
+            return res.status(404).send('Group not found.');
         }
         else {
-            if (groups.deletedCount === 0) {
-                return res.status(404).send('Group not found.');
-            }
-            else {
-                logger.debug(`Success: Deleted group with id ${ req.params.stixId }`);
-                return res.status(204).end();
-            }
+            logger.debug(`Success: Deleted group with id ${ req.params.stixId }`);
+            return res.status(204).end();
         }
-    });
+    }  catch (err) {
+        logger.error('Delete group failed. ' + err);
+        return res.status(500).send('Unable to delete group. Server error.');
+    }
+
 };
