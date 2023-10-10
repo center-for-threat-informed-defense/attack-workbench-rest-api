130346c178e6461edce725634539c29f308d9283
Sun <vsun@mitre.org>
2023-10-10 14:14:56 -0400

    refactoring notes service

--- app/services/notes-service.js
+++ app/services/notes-service.js
@@ -18,264 +18,82 @@ const errors = {
 };
 exports.errors = errors;
 
-exports.retrieveAll = function(options, callback) {
-    // Build the query
-    const query = {};
-    if (!options.includeRevoked) {
-        query['stix.revoked'] = { $in: [null, false] };
-    }
-    if (!options.includeDeprecated) {
-        query['stix.x_mitre_deprecated'] = { $in: [null, false] };
-    }
-    if (typeof options.state !== 'undefined') {
-        if (Array.isArray(options.state)) {
-            query['workspace.workflow.state'] = { $in: options.state };
-        }
-        else {
-            query['workspace.workflow.state'] = options.state;
-        }
-    }
-    if (typeof options.lastUpdatedBy !== 'undefined') {
-      query['workspace.workflow.created_by_user_account'] = lastUpdatedByQueryHelper(options.lastUpdatedBy);
+const NoteRepository = require('../repository/note-repository');
+
+const BaseService = require('./_base.service');
+
+class NoteService extends BaseService {
+
+    constructor() {
+        super(NoteRepository, Matrix);
     }
 
-    // Build the aggregation
-    // - Group the documents by stix.id, sorted by stix.modified
-    // - Use the first document in each note (according to the value of stix.modified)
-    // - Then apply query, skip and limit options
-    const aggregation = [
-        { $sort: { 'stix.id': 1, 'stix.modified': -1 } },
-        { $group: { _id: '$stix.id', document: { $first: '$$ROOT' }}},
-        { $replaceRoot: { newRoot: '$document' }},
-        { $sort: { 'stix.id': 1 }},
-        { $match: query }
-    ];
+    //  async create(data, options) {
+    //     // This function handles two use cases:
+    //     //   1. This is a completely new object. Create a new object and generate the stix.id if not already
+    //     //      provided. Set both stix.created_by_ref and stix.x_mitre_modified_by_ref to the organization identity.
+    //     //   2. This is a new version of an existing object. Create a new object with the specified id.
+    //     //      Set stix.x_mitre_modified_by_ref to the organization identity.
 
-    if (typeof options.search !== 'undefined') {
-        options.search = regexValidator.sanitizeRegex(options.search);
-        const match = { $match: { $or: [
-                    { 'stix.abstract': { '$regex': options.search, '$options': 'i' }},
-                    { 'stix.content': { '$regex': options.search, '$options': 'i' }}
-                ]}};
-        aggregation.push(match);
-    }
+    //     // Create the document
+    //     const note = new Note(data);
 
-    const facet = {
-        $facet: {
-            totalCount: [ { $count: 'totalCount' }],
-            documents: [ ]
-        }
-    };
-    if (options.offset) {
-        facet.$facet.documents.push({ $skip: options.offset });
-    }
-    else {
-        facet.$facet.documents.push({ $skip: 0 });
-    }
-    if (options.limit) {
-        facet.$facet.documents.push({ $limit: options.limit });
-    }
-    aggregation.push(facet);
+    //     options = options || {};
+    //     if (!options.import) {
+    //         // Set the ATT&CK Spec Version
+    //         note.stix.x_mitre_attack_spec_version = note.stix.x_mitre_attack_spec_version ?? config.app.attackSpecVersion;
 
-    // Retrieve the documents
-    Note.aggregate(aggregation, function(err, results) {
-        if (err) {
-            return callback(err);
-        }
-        else {
-            identitiesService.addCreatedByAndModifiedByIdentitiesToAll(results[0].documents)
-                .then(function() {
-                    if (options.includePagination) {
-                        let derivedTotalCount = 0;
-                        if (results[0].totalCount.length > 0) {
-                            derivedTotalCount = results[0].totalCount[0].totalCount;
-                        }
-                        const returnValue = {
-                            pagination: {
-                                total: derivedTotalCount,
-                                offset: options.offset,
-                                limit: options.limit
-                            },
-                            data: results[0].documents
-                        };
-                        return callback(null, returnValue);
-                    }
-                    else {
-                        return callback(null, results[0].documents);
-                    }
-                });
-        }
-    });
-};
+    //         // Record the user account that created the object
+    //         if (options.userAccountId) {
+    //             note.workspace.workflow.created_by_user_account = options.userAccountId;
+    //         }
 
-exports.retrieveById = function(stixId, options, callback) {
-    // versions=all Retrieve all notes with the stixId
-    // versions=latest Retrieve the notes with the latest modified date for this stixId
+    //         // Set the default marking definitions
+    //         await attackObjectsService.setDefaultMarkingDefinitions(note);
 
-    if (!stixId) {
-        const error = new Error(errors.missingParameter);
-        error.parameterName = 'stixId';
-        return callback(error);
-    }
+    //         // Get the organization identity
+    //         const organizationIdentityRef = await systemConfigurationService.retrieveOrganizationIdentityRef();
 
-    if (options.versions === 'all') {
-        Note.find({'stix.id': stixId})
-            .lean()
-            .exec(function (err, notes) {
-                if (err) {
-                    if (err.name === 'CastError') {
-                        const error = new Error(errors.badlyFormattedParameter);
-                        error.parameterName = 'stixId';
-                        return callback(error);
-                    } else {
-                        return callback(err);
-                    }
-                } else {
-                    identitiesService.addCreatedByAndModifiedByIdentitiesToAll(notes)
-                        .then(() => callback(null, notes));
-                }
-            });
-    }
-    else if (options.versions === 'latest') {
-        Note.findOne({ 'stix.id': stixId })
-            .sort('-stix.modified')
-            .lean()
-            .exec(function(err, note) {
-                if (err) {
-                    if (err.name === 'CastError') {
-                        const error = new Error(errors.badlyFormattedParameter);
-                        error.parameterName = 'stixId';
-                        return callback(error);
-                    }
-                    else {
-                        return callback(err);
-                    }
-                }
-                else {
-                    // Note: document is null if not found
-                    if (note) {
-                        identitiesService.addCreatedByAndModifiedByIdentities(note)
-                            .then(() => callback(null, [ note ]));
-                    }
-                    else {
-                        return callback(null, []);
-                    }
-                }
-            });
-    }
-    else {
-        const error = new Error(errors.invalidQueryStringParameter);
-        error.parameterName = 'versions';
-        return callback(error);
-    }
-};
+    //         // Check for an existing object
+    //         let existingObject;
+    //         if (note.stix.id) {
+    //             existingObject = await Note.findOne({ 'stix.id': note.stix.id });
+    //         }
 
-exports.retrieveVersionById = function(stixId, modified, callback) {
-    // Retrieve the versions of the note with the matching stixId and modified date
+    //         if (existingObject) {
+    //             // New version of an existing object
+    //             // Only set the x_mitre_modified_by_ref property
+    //             note.stix.x_mitre_modified_by_ref = organizationIdentityRef;
+    //         }
+    //         else {
+    //             // New object
+    //             // Assign a new STIX id if not already provided
+    //             note.stix.id = note.stix.id || `note--${uuid.v4()}`;
 
-    if (!stixId) {
-        const error = new Error(errors.missingParameter);
-        error.parameterName = 'stixId';
-        return callback(error);
-    }
+    //             // Set the created_by_ref and x_mitre_modified_by_ref properties
+    //             note.stix.created_by_ref = organizationIdentityRef;
+    //             note.stix.x_mitre_modified_by_ref = organizationIdentityRef;
+    //         }
+    //     }
 
-    if (!modified) {
-        const error = new Error(errors.missingParameter);
-        error.parameterName = 'modified';
-        return callback(error);
-    }
+    //     // Save the document in the database
+    //     try {
+    //         const savedNote = await note.save();
+    //         return savedNote;
+    //     }
+    //     catch(err) {
+    //         if (err.name === 'MongoServerError' && err.code === 11000) {
+    //             // 11000 = Duplicate index
+    //             const error = new Error(errors.duplicateId);
+    //             throw error;
+    //         }
+    //         else {
+    //             throw err;
+    //         }
+    //     }
+    // };
 
-    Note.findOne({ 'stix.id': stixId, 'stix.modified': modified }, function(err, note) {
-        if (err) {
-            if (err.name === 'CastError') {
-                const error = new Error(errors.badlyFormattedParameter);
-                error.parameterName = 'stixId';
-                return callback(error);
-            }
-            else {
-                return callback(err);
-            }
-        }
-        else {
-            // Note: document is null if not found
-            if (note) {
-                identitiesService.addCreatedByAndModifiedByIdentities(note)
-                    .then(() => callback(null, note));
-            }
-            else {
-                return callback();
-            }
-        }
-    });
-};
-
-exports.createIsAsync = true;
-exports.create = async function(data, options) {
-    // This function handles two use cases:
-    //   1. This is a completely new object. Create a new object and generate the stix.id if not already
-    //      provided. Set both stix.created_by_ref and stix.x_mitre_modified_by_ref to the organization identity.
-    //   2. This is a new version of an existing object. Create a new object with the specified id.
-    //      Set stix.x_mitre_modified_by_ref to the organization identity.
-
-    // Create the document
-    const note = new Note(data);
-
-    options = options || {};
-    if (!options.import) {
-        // Set the ATT&CK Spec Version
-        note.stix.x_mitre_attack_spec_version = note.stix.x_mitre_attack_spec_version ?? config.app.attackSpecVersion;
-
-        // Record the user account that created the object
-        if (options.userAccountId) {
-            note.workspace.workflow.created_by_user_account = options.userAccountId;
-        }
-
-        // Set the default marking definitions
-        await attackObjectsService.setDefaultMarkingDefinitions(note);
-
-        // Get the organization identity
-        const organizationIdentityRef = await systemConfigurationService.retrieveOrganizationIdentityRef();
-
-        // Check for an existing object
-        let existingObject;
-        if (note.stix.id) {
-            existingObject = await Note.findOne({ 'stix.id': note.stix.id });
-        }
-
-        if (existingObject) {
-            // New version of an existing object
-            // Only set the x_mitre_modified_by_ref property
-            note.stix.x_mitre_modified_by_ref = organizationIdentityRef;
-        }
-        else {
-            // New object
-            // Assign a new STIX id if not already provided
-            note.stix.id = note.stix.id || `note--${uuid.v4()}`;
-
-            // Set the created_by_ref and x_mitre_modified_by_ref properties
-            note.stix.created_by_ref = organizationIdentityRef;
-            note.stix.x_mitre_modified_by_ref = organizationIdentityRef;
-        }
-    }
-
-    // Save the document in the database
-    try {
-        const savedNote = await note.save();
-        return savedNote;
-    }
-    catch(err) {
-        if (err.name === 'MongoServerError' && err.code === 11000) {
-            // 11000 = Duplicate index
-            const error = new Error(errors.duplicateId);
-            throw error;
-        }
-        else {
-            throw err;
-        }
-    }
-};
-
-exports.updateVersion = function(stixId, stixModified, data, callback) {
+    exports.updateVersion = function(stixId, stixModified, data, callback) {
     if (!stixId) {
         const error = new Error(errors.missingParameter);
         error.parameterName = 'stixId';
@@ -323,43 +141,8 @@ exports.updateVersion = function(stixId, stixModified, data, callback) {
             });
         }
     });
-};
+    };
 
-exports.deleteById = function (stixId, callback) {
-    if (!stixId) {
-        const error = new Error(errors.missingParameter);
-        error.parameterName = 'stixId';
-        return callback(error);
-    }
+}
 
-    Note.deleteMany({ 'stix.id': stixId }, function (err, results) {
-        if (err) {
-            return callback(err);
-        } else {
-            return callback(null, results);
-        }
-    });
-};
-
-exports.deleteVersionById = function (stixId, stixModified, callback) {
-    if (!stixId) {
-        const error = new Error(errors.missingParameter);
-        error.parameterName = 'stixId';
-        return callback(error);
-    }
-
-    if (!stixModified) {
-        const error = new Error(errors.missingParameter);
-        error.parameterName = 'modified';
-        return callback(error);
-    }
-
-    Note.findOneAndRemove({ 'stix.id': stixId, 'stix.modified': stixModified }, function (err, note) {
-        if (err) {
-            return callback(err);
-        } else {
-            //Note: note is null if not found
-            return callback(null, note);
-        }
-    });
-};
+module.exports = new NoteService();
