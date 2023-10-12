28faef7521a18cdd706361e93b11dc101e37119e
Sun <vsun@mitre.org>
2023-10-12 17:36:47 -0400

    fixing tests issues

--- app/services/references-service.js
+++ app/services/references-service.js
@@ -5,12 +5,46 @@ const Reference = require('../models/reference-model');
 const ReferenceRepository = require('../repository/references-repository');
 
 const BaseService = require('./_base.service');
+const { DuplicateIdError, BadlyFormattedParameterError, InvalidQueryStringParameterError } = require('../exceptions');
 
 class ReferencesService extends BaseService {
 
     constructor() {
         super(ReferenceRepository, Reference);
     }
+
+    async update(data) {
+        // Note: source_name is used as the key and cannot be updated
+        if (!data.source_name) {
+            const error = new Error(errors.missingParameter);
+            error.parameterName = 'source_name';
+            throw error;
+        }
+    
+        try {
+            const document = await Reference.findOne({ 'source_name': data.source_name });
+            if (!document) {
+                // document not found
+                return null;
+            }
+            else {
+                // Copy data to found document and save
+                Object.assign(document, data);
+                const savedDocument = await document.save();
+                return savedDocument;
+            }
+        }
+        catch(err) {
+            if (err.name === 'CastError') {
+                throw new BadlyFormattedParameterError;
+            }
+            else  if (err.name === 'MongoServerError' && err.code === 11000) {
+                throw new DuplicateIdError;
+            } else {
+                throw err;
+            }
+        }
+    };
 
     async deleteBySourceName(sourceName) {
         if (!sourceName) {
