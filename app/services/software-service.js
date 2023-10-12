4348d217a698b36e070374fb36752cedc09387c9
Sun <vsun@mitre.org>
2023-10-12 15:25:14 -0400

    fixing lint issues

--- app/services/software-service.js
+++ app/services/software-service.js
@@ -3,13 +3,10 @@
 const uuid = require('uuid');
 const Software = require('../models/software-model');
 const systemConfigurationService = require('./system-configuration-service');
-const identitiesService = require('./identities-service');
 const attackObjectsService = require('./attack-objects-service');
 const config = require('../config/config');
-const regexValidator = require('../lib/regex');
-const {lastUpdatedByQueryHelper} = require('../lib/request-parameter-helper');
 
-const { MissingParameterError, MissingPropertyError, PropertyNotAllowedError, BadlyFormattedParameterError, DuplicateIdError, NotFoundError, InvalidQueryStringParameterError } = require('../exceptions');
+const { PropertyNotAllowedError, DuplicateIdError } = require('../exceptions');
 
 const BaseService = require('./_base.service');
 const SoftwareRepository = require('../repository/software-repository');
@@ -94,7 +91,7 @@ class SoftwareService extends BaseService {
                 throw err;
             }
         }
-    };
+    }
 
 }
 
