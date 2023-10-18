497e0c69996df14f82feb132867f0dc88305fcb5
Sun <vsun@mitre.org>
2023-10-18 13:37:32 -0400

    refactored techniques controller

--- app/controllers/techniques-controller.js
+++ app/controllers/techniques-controller.js
@@ -2,8 +2,9 @@
 
 const techniquesService = require('../services/techniques-service');
 const logger = require('../lib/logger');
+const { BadlyFormattedParameterError, InvalidQueryStringParameterError, DuplicateIdError } = require('../exceptions');
 
-exports.retrieveAll = function(req, res) {
+exports.retrieveAll = async function(req, res) {
     const options = {
         offset: req.query.offset || 0,
         limit: req.query.limit || 0,
@@ -17,67 +18,54 @@ exports.retrieveAll = function(req, res) {
         includePagination: req.query.includePagination
     };
 
-    techniquesService.retrieveAll(options, function(err, results) {
-        if (err) {
-            logger.error('Failed with error: ' + err);
-            return res.status(500).send('Unable to get techniques. Server error.');
+    try {
+        const results = await techniquesService.retrieveAll(options);
+        if (options.includePagination) {
+            logger.debug(`Success: Retrieved ${ results.data.length } of ${ results.pagination.total } total technique(s)`);
         }
         else {
-            if (options.includePagination) {
-                logger.debug(`Success: Retrieved ${ results.data.length } of ${ results.pagination.total } total technique(s)`);
-            }
-            else {
-                logger.debug(`Success: Retrieved ${ results.length } technique(s)`);
-            }
-            return res.status(200).send(results);
+            logger.debug(`Success: Retrieved ${ results.length } technique(s)`);
         }
-    });
+        return res.status(200).send(results);
+    }  catch (err) {
+        logger.error('Failed with error: ' + err);
+        return res.status(500).send('Unable to get techniques. Server error.');
+    }
 };
 
-exports.retrieveById = function(req, res) {
+exports.retrieveById = async function(req, res) {
     const options = {
         versions: req.query.versions || 'latest'
     }
-
-    techniquesService.retrieveById(req.params.stixId, options, function (err, techniques) {
-        if (err) {
-            if (err.message === techniquesService.errors.badlyFormattedParameter) {
-                logger.warn('Badly formatted stix id: ' + req.params.stixId);
-                return res.status(400).send('Stix id is badly formatted.');
-            }
-            else if (err.message === techniquesService.errors.invalidQueryStringParameter) {
-                logger.warn('Invalid query string: versions=' + req.query.versions);
-                return res.status(400).send('Query string parameter versions is invalid.');
-            }
-            else {
-                logger.error('Failed with error: ' + err);
-                return res.status(500).send('Unable to get techniques. Server error.');
-            }
+    try {
+        const techniques = await techniquesService.retrieveById(req.params.stixId, options);
+        if (techniques.length === 0) {
+            return res.status(404).send('Technique not found.');
         }
         else {
-            if (techniques.length === 0) {
-                return res.status(404).send('Technique not found.');
-            }
-            else {
-                logger.debug(`Success: Retrieved ${ techniques.length } technique(s) with id ${ req.params.stixId }`);
-                return res.status(200).send(techniques);
-            }
+            logger.debug(`Success: Retrieved ${ techniques.length } technique(s) with id ${ req.params.stixId }`);
+            return res.status(200).send(techniques);
         }
-    });
+    } catch (err) {
+        if (err instanceof BadlyFormattedParameterError) {
+            logger.warn('Badly formatted stix id: ' + req.params.stixId);
+            return res.status(400).send('Stix id is badly formatted.');
+        }
+        else if (err instanceof InvalidQueryStringParameterError) {
+            logger.warn('Invalid query string: versions=' + req.query.versions);
+            return res.status(400).send('Query string parameter versions is invalid.');
+        }
+        else {
+            logger.error('Failed with error: ' + err);
+            return res.status(500).send('Unable to get techniques. Server error.');
+        }
+    }
 };
 
-exports.retrieveVersionById = function(req, res) {
-    techniquesService.retrieveVersionById(req.params.stixId, req.params.modified, function (err, technique) {
-        if (err) {
-            if (err.message === techniquesService.errors.badlyFormattedParameter) {
-                logger.warn('Badly formatted stix id: ' + req.params.stixId);
-                return res.status(400).send('Stix id is badly formatted.');
-            }
-            else {
-                logger.error('Failed with error: ' + err);
-                return res.status(500).send('Unable to get technique. Server error.');
-            }
-        } else {
+exports.retrieveVersionById = async function(req, res) {
+
+        try {
+            const technique =  await techniquesService.retrieveVersionById(req.params.stixId, req.params.modified);
             if (!technique) {
                 return res.status(404).send('Technique not found.');
             }
@@ -85,27 +73,35 @@ exports.retrieveVersionById = function(req, res) {
                 logger.debug(`Success: Retrieved technique with id ${technique.id}`);
                 return res.status(200).send(technique);
             }
-        }
-    });
+        } catch (err) {
+            if (err instanceof BadlyFormattedParameterError) {
+                logger.warn('Badly formatted stix id: ' + req.params.stixId);
+                return res.status(400).send('Stix id is badly formatted.');
+            }
+            else {
+                logger.error('Failed with error: ' + err);
+                return res.status(500).send('Unable to get technique. Server error.');
+            }
+        } 
 };
 
 exports.create = async function(req, res) {
     // Get the data from the request
     const techniqueData = req.body;
+    const options = {
+        import: false,
+        userAccountId: req.user?.userAccountId
+    };
 
     // Create the technique
     try {
-        const options = {
-            import: false,
-            userAccountId: req.user?.userAccountId
-        };
         const technique = await techniquesService.create(techniqueData, options);
 
         logger.debug("Success: Created technique with id " + technique.stix.id);
         return res.status(201).send(technique);
     }
     catch(err) {
-        if (err.message === techniquesService.errors.duplicateId) {
+        if (err instanceof DuplicateIdError) {
             logger.warn("Duplicate stix.id and stix.modified");
             return res.status(409).send('Unable to create technique. Duplicate stix.id and stix.modified properties.');
         }
@@ -116,58 +112,56 @@ exports.create = async function(req, res) {
     }
 };
 
-exports.updateFull = function(req, res) {
+exports.updateFull = async function(req, res) {
     // Get the data from the request
     const techniqueData = req.body;
 
-    // Create the technique
-    techniquesService.updateFull(req.params.stixId, req.params.modified, techniqueData, function(err, technique) {
-        if (err) {
-            logger.error("Failed with error: " + err);
-            return res.status(500).send("Unable to update technique. Server error.");
-        }
-        else {
-            if (!technique) {
-                return res.status(404).send('Technique not found.');
-            } else {
-                logger.debug("Success: Updated technique with id " + technique.stix.id);
-                return res.status(200).send(technique);
-            }
-        }
-    });
-};
-
-exports.deleteVersionById = function(req, res) {
-    techniquesService.deleteVersionById(req.params.stixId, req.params.modified, function (err, technique) {
-        if (err) {
-            logger.error('Delete technique failed. ' + err);
-            return res.status(500).send('Unable to delete technique. Server error.');
-        }
-        else {
-            if (!technique) {
-                return res.status(404).send('Technique not found.');
-            } else {
-                logger.debug("Success: Deleted technique with id " + technique.stix.id);
-                return res.status(204).end();
-            }
-        }
-    });
-};
-
-exports.deleteById = function(req, res) {
-    techniquesService.deleteById(req.params.stixId, function (err, techniques) {
-        if (err) {
-            logger.error('Delete technique failed. ' + err);
-            return res.status(500).send('Unable to delete technique. Server error.');
+    try {
+            // Create the technique
+        const technique = await techniquesService.updateFull(req.params.stixId, req.params.modified, techniqueData);
+        if (!technique) {
+            return res.status(404).send('Technique not found.');
         } else {
-            if (techniques.deletedCount === 0) {
-                return res.status(404).send('Technique not found.');
-            } else {
-                logger.debug(`Success: Deleted technique with id ${req.params.stixId}`);
-                return res.status(204).end();
-            }
+            logger.debug("Success: Updated technique with id " + technique.stix.id);
+            return res.status(200).send(technique);
         }
-    });
+    } catch (err) {
+        logger.error("Failed with error: " + err);
+        return res.status(500).send("Unable to update technique. Server error.");
+    }
+};
+
+exports.deleteVersionById = async function(req, res) {
+
+    try {
+        const technique = await techniquesService.deleteVersionById(req.params.stixId, req.params.modified);
+        if (!technique) {
+            return res.status(404).send('Technique not found.');
+        } else {
+            logger.debug("Success: Deleted technique with id " + technique.stix.id);
+            return res.status(204).end();
+        }
+    } catch (err) {
+        logger.error('Delete technique failed. ' + err);
+        return res.status(500).send('Unable to delete technique. Server error.');
+    }
+};
+
+exports.deleteById = async function(req, res) {
+
+    try {
+        const techniques = await techniquesService.deleteById(req.params.stixId);
+        if (techniques.deletedCount === 0) {
+            return res.status(404).send('Technique not found.');
+        } else {
+            logger.debug(`Success: Deleted technique with id ${req.params.stixId}`);
+            return res.status(204).end();
+        }
+    } catch (err) {
+        logger.error('Delete technique failed. ' + err);
+        return res.status(500).send('Unable to delete technique. Server error.');
+    } 
+
 };
 
 exports.retrieveTacticsForTechnique = async function(req, res) {
@@ -188,7 +182,7 @@ exports.retrieveTacticsForTechnique = async function(req, res) {
         }
     }
     catch(err) {
-        if (err.message === techniquesService.errors.badlyFormattedParameter) {
+        if (err instanceof BadlyFormattedParameterError) {
             logger.warn('Badly formatted stix id: ' + req.params.stixId);
             return res.status(400).send('Stix id is badly formatted.');
         }
