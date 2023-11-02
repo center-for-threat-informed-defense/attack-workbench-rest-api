7cf5466fa993fef2a9363a9948aefbb3d30d7797
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-11-02 11:55:04 -0400

    Stop passing Asset model to AssetsService constructor (only need to pass repository)

--- app/services/assets-service.js
+++ app/services/assets-service.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const Asset = require('../models/asset-model');
-
+// const Asset = require('../models/asset-model');
 const assetsRepository = require('../repository/assets-repository');
 
 const BaseService = require('./_base.service');
@@ -9,4 +8,4 @@ const BaseService = require('./_base.service');
 
 class AssetsService extends BaseService { }
 
-module.exports = new AssetsService(assetsRepository, Asset);
+module.exports = new AssetsService(assetsRepository);
