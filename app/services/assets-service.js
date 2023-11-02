8b9bf305b8aaa9a4c1ac3744017b7ede38166e22
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-11-02 11:50:45 -0400

    Remove useless constructor from AssetsService; Pass repository and model at point of instantiation rather than in constructor

--- app/services/assets-service.js
+++ app/services/assets-service.js
@@ -1,15 +1,12 @@
 'use strict';
 
 const Asset = require('../models/asset-model');
+
 const assetsRepository = require('../repository/assets-repository');
 
 const BaseService = require('./_base.service');
 
-class AssetsService extends BaseService {
 
-    constructor() {
-        super(assetsRepository, Asset);
-    }
-}
+class AssetsService extends BaseService { }
 
-module.exports = new AssetsService();
+module.exports = new AssetsService(assetsRepository, Asset);
