92592cefac9f41be84475fc17eede167e7f5305a
Sun <vsun@mitre.org>
2023-12-08 17:06:31 -0500

    refactored controller

--- app/controllers/relationships-controller.js
+++ app/controllers/relationships-controller.js
@@ -2,8 +2,9 @@
 
 const relationshipsService = require('../services/relationships-service');
 const logger = require('../lib/logger');
+const { BadlyFormattedParameterError, InvalidQueryStringParameterError, DuplicateIdError } = require('../exceptions');
 
-exports.retrieveAll = async function(req, res) {
+exports.retrieveAll = async async function(req, res) {
     const options = {
         offset: req.query.offset || 0,
         limit: req.query.limit || 0,
@@ -39,18 +40,26 @@ exports.retrieveAll = async function(req, res) {
     }
 };
 
-exports.retrieveById = function(req, res) {
+exports.retrieveById = async function(req, res) {
     const options = {
         versions: req.query.versions || 'latest'
     }
+        try {
+            const relationships = await relationshipsService.retrieveById(req.params.stixId, options);
 
-    relationshipsService.retrieveById(req.params.stixId, options, function (err, relationships) {
-        if (err) {
-            if (err.message === relationshipsService.errors.badlyFormattedParameter) {
+            if (relationships.length === 0) {
+                return res.status(404).send('Relationship not found.');
+            }
+            else {
+                logger.debug(`Success: Retrieved ${ relationships.length } relationship(s) with id ${ req.params.stixId }`);
+                return res.status(200).send(relationships);
+            }
+        } catch (err) {
+            if (err instanceof BadlyFormattedParameterError) {
                 logger.warn('Badly formatted stix id: ' + req.params.stixId);
                 return res.status(400).send('Stix id is badly formatted.');
             }
-            else if (err.message === relationshipsService.errors.invalidQueryStringParameter) {
+            else if (err instanceof InvalidQueryStringParameterError) {
                 logger.warn('Invalid query string: versions=' + req.query.versions);
                 return res.status(400).send('Query string parameter versions is invalid.');
             }
@@ -59,30 +68,12 @@ exports.retrieveById = function(req, res) {
                 return res.status(500).send('Unable to get relationships. Server error.');
             }
         }
-        else {
-            if (relationships.length === 0) {
-                return res.status(404).send('Relationship not found.');
-            }
-            else {
-                logger.debug(`Success: Retrieved ${ relationships.length } relationship(s) with id ${ req.params.stixId }`);
-                return res.status(200).send(relationships);
-            }
-        }
-    });
 };
 
-exports.retrieveVersionById = function(req, res) {
-    relationshipsService.retrieveVersionById(req.params.stixId, req.params.modified, function (err, relationship) {
-        if (err) {
-            if (err.message === relationshipsService.errors.badlyFormattedParameter) {
-                logger.warn('Badly formatted stix id: ' + req.params.stixId);
-                return res.status(400).send('Stix id is badly formatted.');
-            }
-            else {
-                logger.error('Failed with error: ' + err);
-                return res.status(500).send('Unable to get relationship. Server error.');
-            }
-        } else {
+exports.retrieveVersionById = async function(req, res) {
+
+        try {
+            const relationship = await relationshipsService.retrieveVersionById(req.params.stixId, req.params.modified);
             if (!relationship) {
                 return res.status(404).send('Relationship not found.');
             }
@@ -90,8 +81,16 @@ exports.retrieveVersionById = function(req, res) {
                 logger.debug(`Success: Retrieved relationship with id ${relationship.id}`);
                 return res.status(200).send(relationship);
             }
+        } catch (err) {
+            if (err instanceof BadlyFormattedParameterError) {
+                logger.warn('Badly formatted stix id: ' + req.params.stixId);
+                return res.status(400).send('Stix id is badly formatted.');
+            }
+            else {
+                logger.error('Failed with error: ' + err);
+                return res.status(500).send('Unable to get relationship. Server error.');
+            }
         }
-    });
 };
 
 exports.create = async function(req, res) {
@@ -109,7 +108,7 @@ exports.create = async function(req, res) {
         return res.status(201).send(relationship);
     }
     catch(err) {
-        if (err.message === relationshipsService.errors.duplicateId) {
+        if (err instanceof DuplicateIdError) {
             logger.warn("Duplicate stix.id and stix.modified");
             return res.status(409).send('Unable to create relationship. Duplicate stix.id and stix.modified properties.');
         }
@@ -120,58 +119,54 @@ exports.create = async function(req, res) {
     }
 };
 
-exports.updateFull = function(req, res) {
+exports.updateFull = async function(req, res) {
     // Get the data from the request
     const relationshipData = req.body;
 
     // Create the relationship
-    relationshipsService.updateFull(req.params.stixId, req.params.modified, relationshipData, function(err, relationship) {
-        if (err) {
+    try {
+        const relationship = relationshipsService.updateFull(req.params.stixId, req.params.modified, relationshipData);
+        if (!relationship) {
+            return res.status(404).send('Relationship not found.');
+        } else {
+            logger.debug("Success: Updated relationship with id " + relationship.stix.id);
+            return res.status(200).send(relationship);
+        }
+    } catch (err) {
             logger.error("Failed with error: " + err);
             return res.status(500).send("Unable to update relationship. Server error.");
         }
-        else {
-            if (!relationship) {
-                return res.status(404).send('Relationship not found.');
-            } else {
-                logger.debug("Success: Updated relationship with id " + relationship.stix.id);
-                return res.status(200).send(relationship);
-            }
-        }
-    });
+
 };
 
-exports.deleteVersionById = function(req, res) {
-    relationshipsService.deleteVersionById(req.params.stixId, req.params.modified, function (err, relationship) {
-        if (err) {
+exports.deleteVersionById = async function(req, res) {
+    try {
+        const relationship = await relationshipsService.deleteVersionById(req.params.stixId, req.params.modified);
+        if (!relationship) {
+            return res.status(404).send('Relationship not found.');
+        } else {
+            logger.debug("Success: Deleted relationship with id " + relationship.stix.id);
+            return res.status(204).end();
+        }
+    } catch (err) {
             logger.error('Delete relationship failed. ' + err);
             return res.status(500).send('Unable to delete relationship. Server error.');
         }
-        else {
-            if (!relationship) {
-                return res.status(404).send('Relationship not found.');
-            } else {
-                logger.debug("Success: Deleted relationship with id " + relationship.stix.id);
-                return res.status(204).end();
-            }
-        }
-    });
+
 };
 
-exports.deleteById = function(req, res) {
-    relationshipsService.deleteById(req.params.stixId, function (err, relationships) {
-        if (err) {
+exports.deleteById = async function(req, res) {
+    try {
+        const relationship = await relationshipsService.deleteById(req.params.stixId);
+        if (relationships.deletedCount === 0) {
+            return res.status(404).send('Relationship not found.');
+        }
+        else {
+            logger.debug(`Success: Deleted relationship with id ${ req.params.stixId }`);
+            return res.status(204).end();
+        }
+    } catch (err) {
             logger.error('Delete relationship failed. ' + err);
             return res.status(500).send('Unable to delete relationship. Server error.');
         }
-        else {
-            if (relationships.deletedCount === 0) {
-                return res.status(404).send('Relationship not found.');
-            }
-            else {
-                logger.debug(`Success: Deleted relationship with id ${ req.params.stixId }`);
-                return res.status(204).end();
-            }
-        }
-    });
 };
