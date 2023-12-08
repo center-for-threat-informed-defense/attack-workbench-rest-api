453c31fe1407c854f8749217c70b858d222be71e
Sun <vsun@mitre.org>
2023-12-08 14:33:35 -0500

    proper stix id

--- app/services/identities-service.js
+++ app/services/identities-service.js
@@ -128,4 +128,4 @@ class IdentitiesService extends BaseService {
 
 }
 
-module.exports = new IdentitiesService('x-mitre-identity', identitiesRepository);
+module.exports = new IdentitiesService('identity', identitiesRepository);
