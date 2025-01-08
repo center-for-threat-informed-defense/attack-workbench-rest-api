5ea3ab6bc6e4eb079a292816ec8062d2201beef6
Sean Sica <23294618+seansica@users.noreply.github.com>
2025-01-08 16:22:27 -0500

    refactor: identities-service

--- app/services/identities-service.js
+++ app/services/identities-service.js
@@ -1,30 +1,25 @@
 'use strict';
 
 const uuid = require('uuid');
-const systemConfigurationService = require('./system-configuration-service');
 const config = require('../config/config');
-const userAccountsService = require('./user-accounts-service');
 const identitiesRepository = require('../repository/identities-repository');
 const BaseService = require('./_base.service');
 const { InvalidTypeError } = require('../exceptions');
 const { Identity: IdentityType } = require('../lib/stix-types');
 
 class IdentitiesService extends BaseService {
-  async addCreatedByAndModifiedByIdentitiesToAll(attackObjects) {
-    const identityCache = new Map();
-    const userAccountCache = new Map();
-    for (const attackObject of attackObjects) {
-      // eslint-disable-next-line no-await-in-loop
-      await this.addCreatedByAndModifiedByIdentities(attackObject, identityCache, userAccountCache);
-    }
-  }
-
+  /**
+   * @public
+   * CRUD Operation: Create
+   *
+   * Creates a new identity object
+   *
+   * Override of base class create() because:
+   * 1. Does not set created_by_ref or x_mitre_modified_by_ref
+   * 2. Does not check for existing identity object
+   */
   async create(data, options) {
-    // This function overrides the base class create() because
-    //   1. It does not set the created_by_ref or x_mitre_modified_by_ref properties
-    //   2. It does not check for an existing identity object
-
-    if (data?.stix?.type !== identityType) {
+    if (data?.stix?.type !== IdentityType) {
       throw new InvalidTypeError();
     }
 
@@ -40,15 +35,62 @@ class IdentitiesService extends BaseService {
       }
 
       // Set the default marking definitions
-      await systemConfigurationService.setDefaultMarkingDefinitionsForObject(data);
+      await this.setDefaultMarkingDefinitionsForObject(data);
 
       // Assign a new STIX id if not already provided
       data.stix.id = data.stix.id || `identity--${uuid.v4()}`;
     }
 
     // Save the document in the database
-    const savedIdentity = await this.repository.save(data);
-    return savedIdentity;
+    try {
+      return await this.repository.save(data);
+    } catch (err) {
+      th
+    }
   }
 
+  /**
+   * @public
+   * CRUD Operation: Read
+   * Inherited from BaseService
+   * retrieveAll(options)
+   */
+
+  /**
+   * @public
+   * CRUD Operation: Read
+   * Inherited from BaseService
+   * retrieveById(stixId, options)
+   */
+
+  /**
+   * @public
+   * CRUD Operation: Read
+   * Inherited from BaseService
+   * retrieveVersionById(stixId, modified)
+   */
+
+  /**
+   * @public
+   * CRUD Operation: Update
+   * Inherited from BaseService
+   * updateFull(stixId, modified, data)
+   */
+
+  /**
+   * @public
+   * CRUD Operation: Delete
+   * Inherited from BaseService
+   * deleteVersionById(stixId, modified)
+   */
+
+  /**
+   * @public
+   * CRUD Operation: Delete
+   * Inherited from BaseService
+   * deleteById(stixId)
+   */
+}
+
+// Export an instance of the service
 module.exports = new IdentitiesService(IdentityType, identitiesRepository);
