5b8c4bd28a39ed0d8079fac546bb0774d66a4b3b
Evan Lucchesi Leon <189633144+elucchesileon@users.noreply.github.com>
2025-01-16 15:53:19 -0500

    fix(collections-repository): Add missing Exception imports

--- app/repository/collections-repository.js
+++ app/repository/collections-repository.js
@@ -5,7 +5,11 @@ const Collection = require('../models/collection-model');
 const AttackObject = require('../models/attack-object-model');
 const regexValidator = require('../lib/regex');
 const { lastUpdatedByQueryHelper } = require('../lib/request-parameter-helper');
-const { DatabaseError } = require('../exceptions');
+const {
+  DatabaseError,
+  BadlyFormattedParameterError,
+  DuplicateIdError,
+} = require('../exceptions');
 
 class CollectionRepository extends BaseRepository {
   constructor(model, attackObjectModel) {
