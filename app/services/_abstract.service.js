2a6c65891f424f25550482ee8ecaec5236da6d5e
Sean Sica <23294618+seansica@users.noreply.github.com>
2025-03-04 11:22:30 -0500

    style(abtract services): disable no-unused vars for entire file

--- app/services/_abstract.service.js
+++ app/services/_abstract.service.js
@@ -1,24 +1,22 @@
+/* eslint-disable no-unused-vars */
+// Note there is a bug in eslint where single line comment will not work ^
 'use strict';
 
 const { NotImplementedError } = require('../exceptions');
 
 class AbstractService {
-  // eslint-disable-next-line no-unused-vars
   retrieveAll(options) {
     throw new NotImplementedError(this.constructor.name, 'retrieveAll');
   }
 
-  // eslint-disable-next-line no-unused-vars
   retrieveById(stixId, options) {
     throw new NotImplementedError(this.constructor.name, 'retrieveById');
   }
 
-  // eslint-disable-next-line no-unused-vars
   retrieveVersionById(stixId, modified) {
     throw new NotImplementedError(this.constructor.name, 'retrieveVersionById');
   }
 
-  // eslint-disable-next-line no-unused-vars
   create(data, options) {
     throw new NotImplementedError(this.constructor.name, 'create');
   }
