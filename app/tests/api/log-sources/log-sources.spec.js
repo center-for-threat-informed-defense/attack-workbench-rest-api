bb98cd6a0ba0e059f60c45074970209b2f3cb671
Evan Lucchesi Leon <189633144+elucchesileon@users.noreply.github.com>
2025-07-08 10:15:16 -0400

    chore: more typos

--- app/tests/api/log-sources/log-sources.spec.js
+++ app/tests/api/log-sources/log-sources.spec.js
@@ -32,7 +32,6 @@ const initialObjectData = {
     ],
     object_marking_refs: ['marking-definition--fa42a846-8d90-4e51-bc29-71d5b4802168'],
     created_by_ref: 'identity--c78cb6e5-0c4b-4611-8297-d1b8b55e40b5',
-    labels: ['label1', 'label2'],
     x_mitre_version: '1.0',
     x_mitre_attack_spec_version: '4.0.0',
     x_mitre_log_source_permutations: [
