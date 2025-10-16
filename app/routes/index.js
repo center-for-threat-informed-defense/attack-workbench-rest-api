8829360f0e75d1f0654a76fa609b6c0e0c44534a
Sean Sica <23294618+seansica@users.noreply.github.com>
2025-10-16 12:05:40 -0400

    feat: condition the global OpenApiValidator middleware execution on runtime config parameter
    - The OpenApiValidator.middleware will attach to the global router ONLY if config.validateRequests.withOpenApi is enabled
    - The default value for this config parameter is true, so the change is non-breaking

--- app/routes/index.js
+++ app/routes/index.js
@@ -17,13 +17,15 @@ router.use('/api', bodyParser.json({ limit: '50mb' }));
 router.use('/api', bodyParser.urlencoded({ limit: '1mb', extended: true }));
 
 // Setup request validation
-router.use(
-  OpenApiValidator.middleware({
-    apiSpec: config.openApi.specPath,
-    validateRequests: true,
-    validateResponses: false,
-  }),
-);
+if (config.validateRequests.withOpenApi) {
+  router.use(
+    OpenApiValidator.middleware({
+      apiSpec: config.openApi.specPath,
+      validateRequests: true,
+      validateResponses: false,
+    }),
+  );
+}
 
 // Setup passport middleware
 router.use('/api', authnConfiguration.passportMiddleware());
